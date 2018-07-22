d258bf9646ca556b6a5aefeec9a71270d2b2d4dc
Roman Garcia <romangarciam@gmail.com>
2018-07-22 19:32:14 -0300

    Not compiling...but just to show progress. First idea on REST layer would be to model 2 resources: minefields and sweeps... A very first implementation would use an instance var for current Board, where all sweeps would occur... Also, an initial sweep implementation would allow sweeping...and should detect all posible outcomes: Game Win / Game Lost (bomb explodes) / No bomb found near / N bombs found near...

--- app/controllers/MinesController.scala
+++ app/controllers/MinesController.scala
@@ -1,16 +1,35 @@
 package controllers
 
-import javafx.scene.control
+import java.time.Clock
+import java.util.concurrent.TimeUnit
+
 import javax.inject._
+import play.api.libs.json._
+import play.api.mvc._
+
+import scala.concurrent.duration.Duration
 
 @Singleton
-class MinesController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class MinesController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with BoardJsonFormat {
 
-  def start = Action {
-    Ok(Json.obj("board" -> Json.arr()))
+  private var currentBoard: Option[Board] = None
+
+  def createBoard(h: Int, w: Int) = Action {
+    currentBoard = Some(Board(h, w))
+    Ok(currentBoard)
   }
 
-  def play(x: Int, y: Int) = Action {
+  def getBoard = Action {
+    Ok(currentBoard)
+  }
+
+  def createSweep(x: Int, y: Int) = Action {
+    if (currentBoard.isEmpty) {
+      BadRequest("No minefield was created")
+    } else {
+      val board = currentBoard.get
+      board.
+    }
 
   }
 
@@ -19,4 +38,42 @@ class MinesController @Inject()(cc: ControllerComponents) extends AbstractContro
 sealed trait Cell
 case object Empty extends Cell
 case object Mine extends Cell
-case class Board(cells: Seq[Cell], time: Int)
+
+object Board {
+  def apply(h: Int, w: Int) = {
+    Board(w, h, Seq(), Clock.systemUTC().millis())
+  }
+}
+
+case class Board(width: Int, height: Int, cells: Seq[Cell], startTimeMs: Long) {
+  def elapsedTimeSeconds: Long = Duration(Clock.systemUTC().millis() - startTimeMs, TimeUnit.MILLISECONDS).toSeconds
+
+  def sweep(x: Int, y: Int): SweepResult = {
+    val pos = (y * width) + x
+    cells(pos) match {
+      case Empty if allClear => {
+        findNearBombs(x, y) match {
+          case 0 => Clear
+          case n => CloseCall(n)
+        }
+      }
+      case Empty => Clear
+      case Mine =>
+    }
+
+  }
+
+  private def findNearBombs(x: Int, y: Int): Int = {
+    0
+  }
+}
+
+sealed trait SweepResult
+case object Win
+case object Boom
+case class CloseCall(bombs: Int)
+case object Clear
+
+trait BoardJsonFormat {
+  implicit val boardWrites = Json.writes[Board]
+}
